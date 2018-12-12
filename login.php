6697321fd94ede70e7e53b81d4411681fc6e3307
Lucija Žbulj <lucija.zzz7@gmail.com>
2018-12-12 19:34:03 +0100

    Revert "JS za validaciju registracije"
    This reverts commit 1b12be599575364db5bd36c9ab87988454f5e9d4.

--- login.php
+++ login.php
@@ -1,10 +1,46 @@
-<?php
-require_once 'vendor/autoload.php';
-use Models\Database;
+<!DOCTYPE html>
+<html>
+<head>
 
-$db = new Database;
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="public/css/main.css">
+</head>
+<body>
 
-$twig = new Twig_Environment(new Twig_Loader_Filesystem('public/views'));
+<header>
+    <nav>
+        <div class="main-wrapper">
+          <div class="nav-login">
+              <form>
+                    <input type="text" name="uid" placeholder="username">
+                    <input type="password" name="pwd" placeholder="lozinka">
+                    <button type="submit" name="submit">Prijava</button>
 
-echo $twig->render('login.html.twig', array(
-));
+              </form>
+
+          </div>
+
+        </div>
+
+    </nav>
+</header>
+
+<section class="main-container">
+  <div class="main-wrapper">
+      <h2>Registracija</h2>
+      <form class="signup-form" action="index.inc.php" method="post">
+          <input type="text" name="first" placeholder="Ime">
+          <input type="text" name="last" placeholder="Prezime">
+          <input type="text" name="uid" placeholder="Username">
+          <input type="password" name="pwd" placeholder="Lozinka">
+          <button type="submit" name="submit">Registriraj se</button>
+      </form>
+  </div>
+</section>
+
+</body>
+
+
+
+
+</html>
