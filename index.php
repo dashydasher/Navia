9d38ddbd3bd53e2b6a1acd326cd6edbee0a96e3d
Lucija Žbulj <lucija.zzz7@gmail.com>
2018-12-12 17:45:30 +0100

    Frontend
    M&L

--- index.php
+++ index.php
@@ -8,7 +8,20 @@ $twig = new Twig_Environment(new Twig_Loader_Filesystem('public/views'));
 
 echo $twig->render('index.html.twig', array(
     "tekst" => "radi!",
-));?>
+));
+
+
+
+
+
+
+
+
+
+
+
+
+?>
 
 <!DOCTYPE html>
 <html>
--- login.php
+++ login.php
@@ -12,8 +12,8 @@
         <div class="main-wrapper">
           <div class="nav-login">
               <form>
-                    <input type="text" name="uid" placeholder="username/e-mail">
-                    <input type="password" name="pwd" placeholder="password">
+                    <input type="text" name="uid" placeholder="username">
+                    <input type="password" name="pwd" placeholder="lozinka">
                     <button type="submit" name="submit">Prijava</button>
 
               </form>
@@ -28,11 +28,11 @@
 <section class="main-container">
   <div class="main-wrapper">
       <h2>Registracija</h2>
-      <form class="signup-form">
-          <inputtype="text" name="first" placeholder="Ime">
-          <inputtype="text" name="last" placeholder="Prezime">
-          <inputtype="text" name="uid" placeholder="Username">
-          <inputtype="password" name="pwd" placeholder="Lozinka">
+      <form class="signup-form" action="index.inc.php" method="post">
+          <input type="text" name="first" placeholder="Ime">
+          <input type="text" name="last" placeholder="Prezime">
+          <input type="text" name="uid" placeholder="Username">
+          <input type="password" name="pwd" placeholder="Lozinka">
           <button type="submit" name="submit">Registriraj se</button>
       </form>
   </div>
