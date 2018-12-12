43020804c7625f190dd12bd88f34a51e62310f52
Lucija Žbulj <lucija.zzz7@gmail.com>
2018-12-12 16:41:40 +0100

    signUp i navbar
    by Matea i Lucija

---
+++ login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="public/css/main.css">
+</head>
+<body>
+
+<header>
+    <nav>
+        <div class="main-wrapper">
+          <div class="nav-login">
+              <form>
+                    <input type="text" name="uid" placeholder="username/e-mail">
+                    <input type="password" name="pwd" placeholder="password">
+                    <button type="submit" name="submit">Prijava</button>
+
+              </form>
+
+          </div>
+
+        </div>
+
+    </nav>
+</header>
+
+<section class="main-container">
+  <div class="main-wrapper">
+      <h2>Registracija</h2>
+      <form class="signup-form">
+          <inputtype="text" name="first" placeholder="Ime">
+          <inputtype="text" name="last" placeholder="Prezime">
+          <inputtype="text" name="uid" placeholder="Username">
+          <inputtype="password" name="pwd" placeholder="Lozinka">
+          <button type="submit" name="submit">Registriraj se</button>
+      </form>
+  </div>
+</section>
+
+</body>
+
+
+
+
+</html>
