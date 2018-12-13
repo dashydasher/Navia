7ba50ede7e1f2e86c024516d81fbe31e3b8eccb4
Lucija Žbulj <lucija.zzz7@gmail.com>
2018-12-13 11:33:35 +0100

    Promjene u login.twig

--- public/js/main.js
+++ public/js/main.js
@@ -4,9 +4,7 @@ function validation(){
 
 	if (validateName() && validateName() &&	validateUsername() && validatePassword()){
 		swal("Bravo!", "Uspješno ste registrirani","uspjeh");
-	}else{
-		sweetAlert("Oops...","Nešto je pošlo po zlu", "error");
-	}	
+	}
 
 }
 
@@ -16,22 +14,22 @@ function validateName(){
 	var name = document.getElementById('name').value;
 	var illegalChars = "^[a-zA-Z ]+$";
 	if (name == "") {
-       
+
         error = "Niste unijeli ime!\n";
-        sweetAlert("Oops...","Niste unijeli ime!", "error");
+        sweetAlert("Oops...",error, "error");
         //alert(error);
 
         return false;
- 
- 
+
+
     } else if (illegalChars.test(name)) {
-        
+
         error = "Ime sadrži ilegalne znakove!\n";
-        sweetAlert("Oops...","Ime sadrži ilegalne znakove!", "error");
+        sweetAlert("Oops...",error, "error");
 		//alert(error);
 		return false;
- 
-    } 
+
+    }
 
     return true;
 
@@ -41,21 +39,21 @@ function validateSurname(){
 	var surname = document.getElementById('surname').value;
 	var illegalChars = "^[a-zA-Z ]+$";
 	if (surrname == "") {
-       
+
         error = "Niste unijeli prezime!\n";
-        sweetAlert("Oops...","Niste unijeli prezime!", "error");
+        sweetAlert("Oops...",error, "error");
         //alert(error);
         return false;
- 
- 
+
+
     } else if (illegalChars.test(surname)) {
-        
+
         error = "Prezime sadrži ilegalne znakove!\n";
-        sweetAlert("Oops...","Prezime sadrži ilegalne znakove!", "error");
+        sweetAlert("Oops...",error, "error");
 		//alert(error);
 		return false;
- 
-    } 
+
+    }
 
     return true;
 
@@ -66,27 +64,27 @@ function validateUsername(){
 	var username = document.getElementById('username').value;
 
 	if (username == "") {
-       
+
         error = "Niste unijeli korisničko ime!\n";
-        sweetAlert("Oops...","Niste unijeli korisničko ime!", "error");
+        sweetAlert("Oops...",error, "error");
         //alert(error);
         return false;
- 
+
     } else if ((username.length < 5) || (username.length > 15)) {
-        
+
         error = "Korisničko ime je krive duljine! Skrati ga ili produži - mora imati 5-15 znakova.\n";
-        sweetAlert("Oops...","Korisničko ime je krive duljine! Skrati ga ili produži - mora imati 5-15 znakova.!", "error");
+        sweetAlert("Oops...",error, "error");
 		//alert(error);
 		return false;
- 
+
     } else if (illegalChars.test(username)) {
-        
+
         error = "Korisničko ime sadrži ilegalne znakove!\n";
-        sweetAlert("Oops...","Korisničko ime sadrži ilegalne znakove!", "error");
+        sweetAlert("Oops...",error, "error");
 		//alert(error);
 		return false;
- 
-    } 
+
+    }
 
     return true;
 }
@@ -95,20 +93,20 @@ function validatePassword(){
 	var password = document.getElementById('password').value;
 
 	if (password == "") {
-       
+
         error = "Niste unijeli lozinku!\n";
         sweetAlert("Oops...",error, "error");
         //alert(error);
         return false;
- 
+
     } else if ((password.length < 6)) {
-        
+
         error = "Lozinka je prekratka, treba imati minimalno 6 znakova! \n";
         sweetAlert("Oops...",error, "error");
 		//alert(error);
 		return false;
- 
-    } 
+
+    }
 
     return true;
 }
