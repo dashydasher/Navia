c88875820a9cf1beeed51b18b774cd1b935a9f50
Matea Bardun <matea.bardun@fer.hr>
2018-12-13 13:25:34 +0100

    profesorov view pocetak

---
+++ profesor.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'vendor/autoload.php';
+use Models\Database;
+
+$db = new Database;
+
+$twig = new Twig_Environment(new Twig_Loader_Filesystem('public/views'));
+
+echo $twig->render('profesor.html.twig', array(
+));
