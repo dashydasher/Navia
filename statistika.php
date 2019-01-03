496de045444a4dcfbb0629120d32142224588051
Matea Bardun <matea.bardun@fer.hr>
2019-01-03 17:09:40 +0100

    dodan dio pie charta

--- statistika.php
+++ statistika.php
@@ -2,7 +2,7 @@
 require_once 'vendor/autoload.php';
 use Models\Room;
 
-#$twig = new Twig_Environment(new Twig_Loader_Filesystem('public/views'));
+$twig = new Twig_Environment(new Twig_Loader_Filesystem('public/views'));
 
 $room_id = htmlspecialchars($_GET["id"]);
 $room = new Room;
@@ -15,15 +15,15 @@ if ($result > 0 && $_SESSION["my_id"] == $room->teacher_id) {
     $room->fetch_comments(0);
     $room->fetch_questions(0);
     $room->fetch_moods(0);
-    var_dump($room);
-    /*
+    /*var_dump($room);*/
+
     echo $twig->render('room-stats.html.twig', array(
         "name" => $_SESSION["my_name"],
         "success" => isset($_SESSION["success"]) ? $_SESSION["success"] : null,
         "error_list" => isset($_SESSION["error"]) ? $_SESSION["error"] : null,
         "room" => $room,
     ));
-    */
+
     unset($_SESSION["success"]);
     unset($_SESSION["error"]);
 } else {
