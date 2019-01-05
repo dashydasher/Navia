58ec16093c6b8ec5303e2797f45067998eb19fc1
pjalusic <petar.jalusic@fer.hr>
2019-01-05 18:53:36 +0100

    početak filtriranja po vremenu. trenutni interval je 15 min (lako se promeni)

---
+++ public/js/room-stats.js
@@ -0,0 +1,137 @@
+var hour_start = parseInt($("#time-start").text().split(":")[0]);
+var time_start = hour_start * 60 + parseInt($("#time-start").text().split(":")[1]);
+
+var hour_end = parseInt($("#time-end").text().split(":")[0]);
+var time_end = hour_end * 60 + parseInt($("#time-end").text().split(":")[1]);
+
+$("#slider-range").slider({
+    min: time_start,
+    max: time_end,
+    step: 15,
+    values: [
+        time_end
+    ],
+    slide: function(e, ui) {
+        $('.slider-time').each(function(i) {
+            var hours = ("00" + Math.floor(ui.values[i] / 60)).slice(-2);
+            var mins = ("00" + (
+                ui.values[i] - (hours * 60)
+            )).slice(-2);
+            hours %= 24;
+            $(this).html(hours + ':' + mins);
+
+            $("#pozRazlozi > table tr:gt(0)").each(function () {
+                var id = $(this).data("id");
+                var parent_id = $(this).data("parent-mood-id");
+                console.log(id + " " + parent_id);
+            });
+        });
+    }
+});
+
+$("#end").on("click", function() {
+    $("#slider-range").slider("values", 0, time_end);
+    $(".slider-time").html(hour_end % 24 + ":" + time_end % 60);
+});
+
+// Load the Visualization API and the corechart package.
+google.charts.load('current', {
+    packages: ['corechart'],
+    callback: drawChart
+});
+
+// Set a callback to run when the Google Visualization API is loaded.
+//  google.charts.setOnLoadCallback(drawChart);
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart() {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+
+
+    var data = new google.visualization.DataTable({
+        cols: [{
+                label: 'Raspolozenja',
+                type: 'string'
+            },
+            {
+                label: 'Ljudi',
+                type: 'number'
+            }
+        ]
+    });
+
+    // get html table rows
+    var raspolozenja = document.getElementById('proba');
+
+    Array.prototype.forEach.call(raspolozenja.rows, function(row) {
+        // exclude column heading
+        if (row.rowIndex > 0) {
+            data.addRow([{
+                    v: (row.cells[0].textContent || row.cells[0].innerHTML).trim()
+                },
+                {
+                    v: 1
+                }
+            ]);
+        }
+    });
+
+    var dataSummary = google.visualization.data.group(
+        data,
+        [0],
+        [{
+            'column': 1,
+            'aggregation': google.visualization.data.sum,
+            'type': 'number'
+        }]
+    );
+
+    // Set chart options
+    var options = {
+        'title': 'Raspoloženja',
+        'width': 700,
+        'height': 500,
+        'fontSize': 25,
+        'colors': ['#f0ad4e', '#5cb85c', '#d9534f']
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+
+    function selectHandler() {
+        var selectedItem = chart.getSelection()[0];
+
+        if (selectedItem) {
+            //console.log(selectedItem);
+            switch (selectedItem.row) {
+                case 1:
+                    $('#pozRazlozi').show();
+                    $('#neutrRazlozi').hide();
+                    $('#negRazlozi').hide();
+                    break;
+                case 0:
+                    $('#pozRazlozi').hide();
+                    $('#neutrRazlozi').show();
+                    $('#negRazlozi').hide();
+                    break;
+                case 2:
+                    $('#pozRazlozi').hide();
+                    $('#neutrRazlozi').hide();
+                    $('#negRazlozi').show();
+                    break;
+                default:
+                    $('#pozRazlozi').hide();
+                    $('#neutrRazlozi').hide();
+                    $('#negRazlozi').hide();
+                    break;
+            }
+        }
+    }
+
+    // Instantiate and draw our chart, passing in some options.
+    google.visualization.events.addListener(chart, 'select', selectHandler);
+    chart.draw(dataSummary, options);
+}
--- statistika.php
+++ statistika.php
@@ -15,13 +15,88 @@ if ($result > 0 && $_SESSION["my_id"] == $room->teacher_id) {
     $room->fetch_comments(0);
     $room->fetch_questions(0);
     $room->fetch_moods(0);
-    /*var_dump($room);*/
+
+    // početno vrijeme bude kad se pojavi prvo raspoloženje
+    // zadnje vrijeme je kad se pojavi zadnje raspoloženje
+    $min_time = null;
+    $max_time = null;
+    foreach ($room->moods as $mood) {
+        $mood_time = new DateTime($mood->time);
+        if ($min_time === null || $mood_time < $min_time) {
+            $min_time = $mood_time;
+        }
+        if ($max_time === null || $mood_time > $max_time) {
+            $max_time = $mood_time;
+        }
+    }
+    $pocetno_vrijeme = $min_time;
+    $maksimalno_vrijeme = $max_time;
+
+    // kolko česti da budu intervali
+    $minute_interval = 15;
+
+    $minutes = date('i', strtotime($min_time->format('Y-m-d H:i:s')));
+    $oduzmi_minute = intval($minutes - intval($minutes / $minute_interval) * $minute_interval);
+    // prvi interval je ustvari n*15 minuta od punog sata, ali tako da to obuhvaća prvo raspoloženje
+    // ako je prvo raspoloženje u 18:31 onda je prvi interval u 18:45
+    // ovjde samo dobivamo 18:30, a tek u petlji to povećavamo za 15 min
+    $pocetno_vrijeme->sub(new DateInterval('PT' . $oduzmi_minute . 'M'));
+
+    // sluzi za petlju
+    $trenutno_vrijeme = new DateTime($pocetno_vrijeme->format('Y-m-d H:i:s'));
+
+    $prikazi_moodove = array();
+
+    // ova petlja je nesto skracena jer koristi varijablu $my_moods koja svaki put
+    // izbaci mood iz sebe kad se taj mood obradi
+    $my_moods = $room->moods;
+
+    $prva_petlja = true;
+    while($trenutno_vrijeme < $maksimalno_vrijeme) {
+        if (!$prva_petlja) {
+            $prethodni_interval = new DateTime($trenutno_vrijeme->format('Y-m-d H:i:s'));
+        } else {
+            // donji interval je "beskonačan"
+            $prethodni_interval = new DateTime('1970-01-01 00:00:00');
+            $pocetno_vrijeme->add(new DateInterval('PT' . $minute_interval . 'M'));
+            $prva_petlja = false;
+        }
+        $trenutno_vrijeme->add(new DateInterval('PT' . $minute_interval . 'M'));
+
+        $interval_key = $trenutno_vrijeme->format('H:i');
+
+        foreach ($my_moods as $mood_key => $mood) {
+            $mood_time = new DateTime($mood->time);
+
+            if ($mood_time > $prethodni_interval && $mood_time <= $trenutno_vrijeme) {
+                #$prikazi_moodove[$mood->id] = $mood;
+                $prikazi_moodove[$mood->id] = $mood->parent_mood_id;
+
+                // ako ima roditelja onda roditelja nemoj u budućim intervalima prikazivati jer
+                // je on preuzeo njegovu ulogu
+                if ($mood->parent_mood_id) {
+                    unset($prikazi_moodove[$mood->parent_mood_id]);
+                }
+
+                // izbaci ga iz polja da se ponovo ne provjerava
+                unset($my_moods[$mood_key]);
+            }
+        }
+        $moods_intervals[$interval_key] = $prikazi_moodove;
+    }
+
+    var_dump($pocetno_vrijeme->format('H:i'));
+    var_dump($maksimalno_vrijeme->format('H:i'));
+    var_dump($moods_intervals);
 
     echo $twig->render('room-stats.html.twig', array(
         "name" => $_SESSION["my_name"],
         "success" => isset($_SESSION["success"]) ? $_SESSION["success"] : null,
         "error_list" => isset($_SESSION["error"]) ? $_SESSION["error"] : null,
         "room" => $room,
+        "moods_intervals" => $moods_intervals,
+        "time_start" => $pocetno_vrijeme->format('H:i'),
+        "time_end" => $maksimalno_vrijeme->format('H:i'),
     ));
 
     unset($_SESSION["success"]);
