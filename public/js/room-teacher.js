f2d31d9a34ed0f196ee6f63b8667c053a1921040
pjalusic <petar.jalusic@fer.hr>
2019-01-09 16:21:36 +0100

    kroz sucelje za prikaz na projektoru, brisanje komentara / pitanja: JS dio samo

--- public/js/room-teacher.js
+++ public/js/room-teacher.js
@@ -4,8 +4,8 @@ var pause = false;
 var last_timestamp;
 var hidden, visibilityChange;
 
-window.onunload = function(){
-    if(typeof request !=='undefined') {
+window.onunload = function() {
+    if (typeof request !== 'undefined') {
         request.abort();
     }
 }
@@ -41,14 +41,14 @@ function oznaci_neprocitane() {
     $("#komentari > li[data-seen='0'], #pitanja > li[data-seen='0']").each(function() {
         // flashaj lol
         $(this).delay(100).fadeIn(350).fadeOut(350).fadeIn(350).fadeOut(350).fadeIn(350).fadeOut(350).fadeIn(350);
-        // ovo drugo se može obrisat
         $(this).attr('data-seen', "1");
+        // ovo se može obrisat valjda
         $(this).data('seen', "1");
     });
 }
 
 /*
-ovdje se oznacuju neprocitani jer se to izvodi kad se profesor vrati na taj tab
+ovdje se oznacuju neprocitani jer se to izvodi tek kad se profesor vrati na taj tab (ako je bio odsutan)
 */
 function poll_again(timestamp) {
     if (!pause && !ajaxInProgress) {
@@ -84,13 +84,23 @@ function get_mood_icon_by_id(mood) {
 
 function dodaj_komentare(komentari) {
     for (var i = 0, len = komentari.length; i < len; i++) {
-        $("#komentari").prepend( $("<li data-seen='0' class='list-group-item list-group-item-success' style='margin: 10px'>").append(komentari[i].signature + ": " + komentari[i].comment) );
+        $("#komentari")
+            .prepend($("<li data-seen='0' data-id='" + komentari[i].id + "' class='list-group-item list-group-item-success' style='margin: 10px'>")
+                .append(komentari[i].signature + ": " + komentari[i].comment)
+                .append($('<button class="btn btn-default btn-md ajaxRemoveComment btn-danger" style="float: right; margin-top: 4px;">')
+                    .append($('<i class="fa fa-times" aria-hidden="true">')))
+            );
     }
 }
 
 function dodaj_pitanja(pitanja) {
     for (var i = 0, len = pitanja.length; i < len; i++) {
-        $("#pitanja").prepend( $("<li data-seen='0' class='list-group-item list-group-item-danger' style='margin: 10px'>").append(pitanja[i].signature+": "+ pitanja[i].question) );
+        $("#pitanja")
+            .prepend($("<li data-seen='0' data-id='" + pitanja[i].id + "' class='list-group-item list-group-item-danger' style='margin: 10px'>")
+                .append(pitanja[i].signature + ": " + pitanja[i].question)
+                .append($('<button class="btn btn-default btn-md ajaxRemoveQuestion btn-danger" style="float: right; margin-top: 4px;">')
+                    .append($('<i class="fa fa-times" aria-hidden="true">')))
+            );
     }
 }
 
@@ -100,7 +110,7 @@ function dodaj_rasposlozenja(moods) {
         if (mood.parent_mood_id) {
             $('#raspolozenja i[data-id="' + mood.parent_mood_id + '"]').remove();
         }
-        $("#raspolozenja").append( get_mood_icon_by_id(mood) );
+        $("#raspolozenja").append(get_mood_icon_by_id(mood));
     }
 }
 
@@ -109,8 +119,10 @@ function poll(timestamp) {
     ajaxInProgress = true;
 
     last_timestamp = timestamp;
-    var queryString = {'timestamp' : timestamp};
-      return $.get("php-api/long-polling.php", queryString)
+    var queryString = {
+        'timestamp': timestamp
+    };
+    return $.get("php-api/long-polling.php", queryString)
         .done(function(data) {
             if (data.success) {
                 dodaj_komentare(data.comments);
@@ -130,124 +142,48 @@ function poll(timestamp) {
         })
 }
 
-$("document").ready(function () {
+$("document").ready(function() {
     poll_again();
 });
 
-google.charts.load('current', {
-      packages: ['corechart'],
-      callback: drawChart
+$('#pitanja').on('click', '.ajaxRemoveQuestion', function() {
+    if (!confirm("Obrisati zapis?")) {
+        return false;
+    }
+    var li = $(this).closest('li');
+    var id = li.data('id');
+    if (id) {
+        var serializedData = {
+            "question_id": id,
+        };
+        $.post("./php-api/question-remove.php", serializedData)
+            .done(function(data) {
+                if (data.success) {
+                    li.remove();
+                } else {
+                    alert(data.error);
+                }
+            });
+    }
 });
 
-
-function drawChart() {
-
-    var data = new google.visualization.DataTable({
-          cols: [
-            { label: 'Raspolozenja', type: 'string' },
-            { label: 'Ljudi', type: 'number' }
-          ]
-      });
-
-    // get html table rows
-    var raspolozenja = document.getElementById('proba');
-
-    Array.prototype.forEach.call(raspolozenja.rows, function(row) {
-// exclude column heading
-        if (row.rowIndex > 0) {
-            data.addRow([
-      { v: (row.cells[0].textContent || row.cells[0].innerHTML).trim() },
-      { v: 1 }
-    ]);
-  }
-  });
-
-var dataSummary = google.visualization.data.group(
-data,
-[0],
-[{'column': 1, 'aggregation': google.visualization.data.sum, 'type': 'number'}]
-);
-
-      // Set chart options
-    var options = {'width':550,
-                   'height':500,
-                   'legend':'bottom',
-                   'fontSize':32,
-                   'colors': ['#5cb85c', '#f0ad4e', '#d9534f']};
-
-  var chart = new google.visualization.PieChart(document.getElementById('chart'));
-
-   function selectHandler()
-   {
-        var selectedItem = chart.getSelection()[0];
-
-       if (selectedItem)
-       {
-          var mood = data.getValue(selectedItem.row, 0);
-          //alert('The user selected ' + mood);
-          switch(mood) {
-            case '1':
-              $('#pozRazlozi').show();
-              $('#negRazlozi').hide();
-              $('#neutrRazlozi').hide();
-              break;
-            case '2':
-              $('#pozRazlozi').hide();
-              $('#negRazlozi').show();
-              $('#neutrRazlozi').hide();
-              break;
-            case '3':
-              $('#pozRazlozi').hide();
-              $('#negRazlozi').hide();
-              $('#neutrRazlozi').show();
-              break;
-            default:
-              $('#pozRazlozi').hide();
-              $('#negRazlozi').hide();
-              $('#neutrRazlozi').hide();
-              break;
-          }
-      }
-   }
-
-    // Instantiate and draw our chart, passing in some options.
-      google.visualization.events.addListener(chart, 'select', selectHandler);
-    chart.draw(dataSummary, options);
-  }
-
-
-
-
-
-$("#razlog-submit").on("click", function(){
-
-var donut_chart = Morris.Donut({
-element: 'chart',
-data: "neki_data_iz_PHPa"
-});
-
-$('#like_form').on('submit', function(event){
-event.preventDefault();
-var checked = $('input[name=emotion]:checked', '#like_form').val();
-if(checked == undefined)
-{
-alert("Molim odaberite neku od emocija!");
-return false;
-}
-else
-{
-var form_data = $(this).serialize();
-$.ajax({
-url:"action.php",
-method:"POST",
-data:form_data,
-dataType:"json",
-success:function(data)
-{
-$('#like_form')[0].reset();
-donut_chart.setData(data);
-}
-});
-}
-});
+$('#komentari').on('click', '.ajaxRemoveComment', function() {
+    if (!confirm("Obrisati zapis?")) {
+        return false;
+    }
+    var li = $(this).closest('li');
+    var id = li.data('id');
+    if (id) {
+        var serializedData = {
+            "comment_id": id,
+        };
+        $.post("./php-api/comment-remove.php", serializedData)
+            .done(function(data) {
+                if (data.success) {
+                    li.remove();
+                } else {
+                    alert(data.error);
+                }
+            });
+    }
 });
